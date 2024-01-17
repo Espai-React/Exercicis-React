f1e8149fcd1bea9adb9d9f7b0dc25cbdf5c54071
llopfilms <llopfilms@gmail.com>
2024-01-17 13:28:32 +0100

    Actualització

---
+++ ReactTS/Byte Grad/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+//module.exports = {
+//  content: ['./src/**/*.{html,js}'],
+//  theme: {
+//    extend: {},
+//  },
+//  plugins: [],
+//};
+
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+    },
+  },
+  plugins: []
+};
