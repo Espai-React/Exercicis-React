a1f7359f9abef18e0609401f1df4a1ffee251c7e
llopfilms <llopfilms@gmail.com>
2024-01-17 14:33:45 +0100

    Actualització

--- ReactTS/Byte Grad/postcss.config.js
+++ ReactTS/Byte Grad/postcss.config.js
@@ -1,13 +1,14 @@
-/* module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}; */
-
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 };
+
+/* module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};*/
+
--- ReactTS/Byte Grad/tailwind.config.js
+++ ReactTS/Byte Grad/tailwind.config.js
@@ -11,8 +11,7 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {
-    },
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 };
