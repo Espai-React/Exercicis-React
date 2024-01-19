6b34d0f4da8a10603dbe8c0ce97d62aa66aad52b
llopFilms <llopFilms@gmail.com>
2024-01-19 12:39:57 +0100

    Actualització

--- ReactTS/Byte Grad/src/App.tsx
+++ ReactTS/Byte Grad/src/App.tsx
@@ -1,13 +1,34 @@
 import './App.css';
-import { Boto1 } from './components/Boto1';
+import { BotoTailwind, BotoCSS } from './components/Boto1';
 
 const App = () => (
-  <div className='min-h-screen bg-gray-800 p-10 rounded-xl flex flex-col gap-6'>
+  <div className='min-h-screen bg-gray-800 p-24 rounded-xl flex flex-col gap-6'>
     <div className='w-full'>
       <h2 className='text-orange-300 text-3xl font-semibold'>React TS</h2>
-      <hr className='mx-auto w-1/2 border-orange-300 border-2 my-2' />
+      <hr className='mx-auto w-2/3 border-orange-300 border-2 my-2' />
     </div>
-    <Boto1 background='blue' fontSize={40} pillShape={true} />
+    <BotoTailwind
+      background='blue-500'
+      color='white'
+      fontSize='20'
+      padding={[6, 4]}
+      disabled={false}
+    />
+    <BotoCSS
+      styles={{
+        background: 'red',
+        color: 'white',
+        fontSize: 20,
+        padding: '2px 4px',
+      }}
+      borderRadius={{
+        borderTopLeftRadius: 4,
+        borderTopRightRadius: 4,
+        borderBottomLeftRadius: 8,
+        borderBottomRightRadius: 8,
+      }}
+      disabled={false}
+    />
   </div>
 );
 
--- ReactTS/Byte Grad/src/components/Boto1.tsx
+++ ReactTS/Byte Grad/src/components/Boto1.tsx
@@ -1,16 +1,50 @@
-export function Boto1({
-  background,
-  fontSize,
-  pillShape,
-}: {
-  background: string;
-  fontSize: number;
-  pillShape: boolean;
-}) {
+type Color = 'blue-500' | 'orange-200' | 'Lime-200';
 
+type BotoTailwindT = {
+  background: Color;
+  color: string;
+  fontSize: string;
+  padding: [number, number];
+  disabled?: boolean;
+};
+
+type BotoCSST = {
+  styles: React.CSSProperties;
+  borderRadius: { [key: string]: number };
+  /*borderRadius: Record<string, number>;*/
+  disabled?: boolean;
+};
+
+/* export function Boto1({ background, color, fontSize, disabled }: ButtonProps) {
   return (
-    <button className='w-24 self-center text-gray-800 rounded px-4 py-2'>
+    <button
+      className={`self-center bg-${background} text-[${fontSize}px] text-${color} rounded px-4 py-2`}
+      disabled={disabled}>
       Click me
     </button>
   );
-}
+} */
+
+const BotoTailwind: React.FC<BotoTailwindT> = ({
+  background,
+  color,
+  fontSize,
+  padding,
+  disabled,
+}) => (
+  <button
+    className={`self-center bg-${background} text-[${fontSize}px] text-${color} rounded px-[${padding[0]}px] py-${padding[1]}`}
+    disabled={disabled}>
+    Click me
+  </button>
+);
+
+export { BotoTailwind };
+
+const BotoCSS: React.FC<BotoCSST> = ({ styles, borderRadius, disabled }) => (
+  <button style={{ ...styles , ...borderRadius }} disabled={disabled}>
+    Click me
+  </button>
+);
+
+export { BotoCSS };
